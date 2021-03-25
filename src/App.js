73bf92a95b0f7eefa71b563b6983c0c5ff60ef74
Noah <noah@eckhardt.ch>
2021-03-25 21:12:37 +0100

    Re-Hello!

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                    Goodbye, World
+                    Re-Hello, World
                 </p>
             </header>
         </div>
