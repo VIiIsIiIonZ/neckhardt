e5675cba19a83c15d1e312a173f2e4e8a6d1aa8d
Noah <noah@eckhardt.ch>
2021-03-25 20:30:50 +0100

    initial commit

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer>
+            <p>This is a footer element which is nested within the App.js file.</p>
+        </footer>
+    )
+}
+
+export default Footer;
