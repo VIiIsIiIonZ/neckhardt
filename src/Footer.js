65916cb8656153a0e1238443d6a2461d1cd4dca1
Noah <noah@eckhardt.ch>
2021-03-25 20:36:43 +0100

    deleted Footer.js

--- src/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <footer>
-            <p>This is a footer element which is nested within the App.js file.</p>
-        </footer>
-    )
-}
-
-export default Footer;
