12691871122fc9f255861ad30e5e4f8f7d52ca23
Noah <noah@eckhardt.ch>
2021-04-10 23:17:16 +0200

    smthn im stuck at idfk

--- src/App.js
+++ src/App.js
@@ -24,6 +24,15 @@ function App(props) {
         const remainingTasks = tasks.filter(task => id !== task.id);
         setTasks(remainingTasks);
     }
+    function editTask(id, newName) {
+        const editedTaskList = tasks.map(task => {
+            if (id === task.id) {
+                return {...tasks, name: newName};
+            }
+            return task;
+        })
+        setTasks(editedTaskList);
+    }
     const taskList = tasks.map(task => (
         <Todo
             id={task.id}
@@ -32,6 +41,7 @@ function App(props) {
             key={task.id}
             toggleTaskCompleted={toggleTaskCompleted}
             deleteTask={deleteTask}
+            editTask={editTask}
         />
     ));
 
